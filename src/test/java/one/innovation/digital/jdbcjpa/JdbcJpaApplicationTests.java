a553e3143af1cedc9e809768d6019534f5c0c63c
Gustavo Lening <gustavo.-godoy@hotmail.com>
2021-06-09 00:09:49 -0300

    primeiro commit

---
+++ src/test/java/one/innovation/digital/jdbcjpa/JdbcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package one.innovation.digital.jdbcjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
